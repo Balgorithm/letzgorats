03dd1e85851fff83f37ec22635c284f8a5d8089d
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2022-01-21 00:12:10 +0900

    큰 수의 법칙(O)

--- Greedy/큰 수의 법칙/큰 수의 법칙.py
+++ Greedy/큰 수의 법칙/큰 수의 법칙.py
@@ -1,13 +1,9 @@
 import sys
-input = sys.stdin.readline
+input = sys.stdin.readline # input()보다 빠르다. 습관화
 
-n, m, k = input()
-numbers = list(map(int,input().split()))
-sorted_numbers = sorted(numbers)[-2:]
-answer = 0
-if k <=2 :
-  print(sorted_numbers[-1] * k)
-else:
-  for num in sorted_numbers:
-    answer += num
-         
+# n,m,k 입력받기 - map, int, input().split() 사용
+n, m, k = map(int,input().split())
+numbers = list(map(int,input().split())) # 숫자 배열 입력받기
+biggest_num, second_num = sorted(numbers)[-1], sorted(numbers)[-2] # 어차피 제일 큰 수랑 그 다음 큰 수 밖에 안 쓰인다.
+answer = (m // k) * k * biggest_num + (m % k) * second_num # 규칙성 찾고 적용
+print(answer) 

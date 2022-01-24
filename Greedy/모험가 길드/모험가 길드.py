fa4a789ebd65015737d9c80ddb5aa1ef139fe1de
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2022-01-24 15:42:47 +0900

    모험가 길드(X)

--- Greedy/모험가 길드/모험가 길드.py
+++ Greedy/모험가 길드/모험가 길드.py
@@ -5,11 +5,28 @@ n = int(input()) # n 입력받기
 fear_list = sorted(list(map(int,input().split()))) # fear_list 정렬
 group = 0 
 idx = 0 # 초기 idx(처음 idx) 0으로 설정
+temp = 0 
 while idx != n: # idx가 fear_list 인덱스 끝까지 갈 때까지
-    if idx + fear_list[idx] >= n:   # idx 범위 예외처리
-        if fear_list[idx] == 1: # fear_list[idx] 값이 1 이면 
-            group += 1  # 현재 가리키는 그 값도 더해줘야 한다.
-        break
-    group += 1 # if 문에 안 걸리면 group 1 증가  
-    idx +=  fear_list[idx] # 현재 가리키는 fear_list[idx] 만큼 idx 증가
+    temp += 1
+    if (temp >= fear_list[idx]):
+        temp = 0
+        group += 1
+    idx += 1 
+print(group)
+
+# 답지 풀이
+import sys
+input = sys.stdin.readline
+
+n = int(input()) # n 입력받기
+fear_list = sorted(list(map(int,input().split()))) # fear_list 정렬
+temp = 0 # 사람 수 
+group = 0 
+idx = 0 # 초기 idx(처음 idx) 0으로 설정
+for fear in fear_list:
+    temp += 1 # 사람 수 1 증가 
+    if temp >= fear: # 사람 수가 해당 공포도와 같아지는 순간 
+        group += 1 # 그룹수 1 증가 
+        temp = 0  # 사람 수 0으로 다시 초기화
+
 print(group)

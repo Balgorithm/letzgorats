4cc39e403adb92d6e59b33b0f3da1360c1868e35
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2022-01-24 14:45:22 +0900

    모험가 길드(O)

--- Greedy/모험가 길드/모험가 길드.py
+++ Greedy/모험가 길드/모험가 길드.py
@@ -6,7 +6,7 @@ fear_list = sorted(list(map(int,input().split())))  # fear_list 정렬
 group = 0
 # 1 2 2 2 3
 idx = 0 # 초기 idx(처음 idx) 0으로 설정
-while idx != (n-1) : # idx가 fear_list 인데스 끝까지 갈 때까지  
+while idx != n : # idx가 fear_list 인데스 끝까지 갈 때까지  
     if idx >= n or idx + fear_list[idx] >= n : # idx 범위 예외처리
         break
     group += 1 # group 1증가 

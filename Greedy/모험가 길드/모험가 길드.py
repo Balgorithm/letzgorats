b8f1ab5ebbee94f84c27e1e9b8bcc2530a117027
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2022-01-24 14:47:13 +0900

    모험가 길드(O)

--- Greedy/모험가 길드/모험가 길드.py
+++ Greedy/모험가 길드/모험가 길드.py
@@ -7,7 +7,7 @@ group = 0
 # 1 2 2 2 3
 idx = 0 # 초기 idx(처음 idx) 0으로 설정
 while idx != n : # idx가 fear_list 인데스 끝까지 갈 때까지  
-    if idx >= n or idx + fear_list[idx] >= n : # idx 범위 예외처리
+    if idx > n or idx + fear_list[idx] > n : # idx 범위 예외처리
         break
     group += 1 # group 1증가 
     idx +=  fear_list[idx]  # 현재 가리키는 fear_list[idx] 만큼 idx를 증가
